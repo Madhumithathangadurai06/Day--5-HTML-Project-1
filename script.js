f37fa441ff5e93d29e128b457a938b9faa31da55
Madhumithathangadurai06 <tmadhumitha015@gmail.com>
2024-07-09 23:17:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById("image").addEventListener("click",function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2020/09/02/05/36/mercury-5537392_640.jpg"
+});
+document.getElementById("Venus").addEventListener("click",function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2011/12/13/14/39/venus-11022_640.jpg"
+});
+document.getElementById("Earth").addEventListener("click",function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2011/12/13/14/28/earth-11009_1280.jpg"
+});
+document.getElementById("Mars").addEventListener("click",function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2022/06/29/18/26/mars-7292339_640.jpg"
+});
+document.getElementById("Jupiter").addEventListener("click",function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2021/10/14/12/02/jupiter-6708922_640.jpg"
+
+});
+document.getElementById("Uranus").addEventListener("click",function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2020/09/10/01/58/uranus-5559037_640.jpg"
+
+});
+document.getElementById("Neptune").addEventListener("click",function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2020/09/06/22/11/neptune-5550216_1280.jpg"
+
+});
+document.getElementById("Saturn").addEventListener("click",function(){
+    
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2016/05/06/08/47/space-1375449_640.png"
+
+});
+
